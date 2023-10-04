93ab17b599ccf04546dac01eb12063067df2295b
Mateo <m.avila@informaciongerencial.com>
2023-10-03 22:43:08 -0500

    commit 2: Vistas de las habitaciones (Martes)

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,24 @@
-import { Modal, Ripple, initTE } from "tw-elements";
-initTE({ Modal, Ripple });
+function openModal(accion) {
+    if (accion == 'eliminarHotel') {
+        document.getElementById("modalEliminarHotel").style.display = "block";
+    } else if (accion == 'editarHotel' || accion == 'agregarHotel') {
+        document.getElementById("modalEditarHotel").style.display = "block";
+    } else if (accion == 'eliminarHabitacion') {
+        document.getElementById("modalEliminarHabitacion").style.display = "block";
+    } else if (accion == 'editarHabitacion' || accion == 'agregarHabitacion') {
+        document.getElementById("modalEditarHabitacion").style.display = "block";
+    }
+}
+
+// Función para cerrar la modal
+function closeModal(accion) {
+    if (accion == 'eliminarHotel') {
+        document.getElementById("modalEliminarHotel").style.display = "none";
+    } else if (accion == 'editarHotel' || accion == 'agregarHotel') {
+        document.getElementById("modalEditarHotel").style.display = "none";
+    }else if (accion == 'eliminarHabitacion') {
+        document.getElementById("modalEliminarHabitacion").style.display = "none";
+    } else if (accion == 'editarHabitacion' || accion == 'agregarHabitacion') {
+        document.getElementById("modalEditarHabitacion").style.display = "none";
+    }
+}
